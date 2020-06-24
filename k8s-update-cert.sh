5811cd2b770799dea19d868bb844c6383b851042
Orlando Brea <orlando.brea@parserdigital.com>
2020-06-24 12:11:40 -0300

    Cambio estructura

--- k8s-update-cert.sh
+++ k8s-update-cert.sh
@@ -15,9 +15,9 @@ echo "Incorporando ca.crt al secret ingress-cert"
 CA_CRT=$(cat ca.crt | base64 | sed 's/[\\&*./+!]/\\&/g' )
 
 echo "Generando manifiesto de secret ingress-cert"
-sed 's/{{CERT}}/'$CA_CRT'/g' 01-update-secret.tpl > 01-update-secret.yaml
+sed 's/{{CERT}}/'$CA_CRT'/g' template/01-update-secret.tpl > template/01-update-secret.yaml
 
 echo "Aplicando manifiesto"
 #kubectl apply -f 01-update-secret.yaml
 
-docker run --rm --name kubectl -v $PWD/kubeconfig:/.kube/config -v $PWD/01-update-secret.yaml:/01-update-secret.yaml bitnami/kubectl:1.16.3 apply -f 01-update-secret.yaml
+docker run --rm --name kubectl -v $PWD/kubeconfig:/.kube/config -v $PWD/template/01-update-secret.yaml:/01-update-secret.yaml bitnami/kubectl:1.16.3 apply -f 01-update-secret.yaml
