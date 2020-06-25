ee90cc6e3f1f9a371884a37d3802510a94ae26e0
Orlando Brea <orlando.brea@parserdigital.com>
2020-06-25 10:39:28 -0300

    Cambio forma de generar manifiesto

--- k8s-update-cert.sh
+++ k8s-update-cert.sh
@@ -12,10 +12,21 @@ echo "Creando ca.crt en base a $1"
 openssl x509 -outform der -in certs/$1 -out certs/ca.crt
 
 echo "Incorporando ca.crt al secret ingress-cert"
-CA_CRT=$(cat certs/ca.crt | base64 | sed 's/[\\&*./+!]/\\&/g' )
+# CA_CRT=$(cat certs/ca.crt | base64 | sed 's/[\\&*./+!]/\\&/g' )
 
-echo "Generando manifiesto de secret ingress-cert"
-sed `s/{{CERT}}/$CA_CRT/g` template/01-update-secret.tpl > template/01-update-secret.yaml
+cat > template/01-update-secret.yaml <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: ingress-cert
+  namespace: dcm4che
+type: Opaque
+data:
+  ca.crt: $(cat certs/ca.crt | base64 | tr -d '\n')
+EOF
+
+# echo "Generando manifiesto de secret ingress-cert"
+# sed `s/{{CERT}}/$CA_CRT/g` template/01-update-secret.tpl > template/01-update-secret.yaml
 
 echo "Aplicando manifiesto"
 #kubectl apply -f 01-update-secret.yaml
--- template/01-update-secret.tpl
+++
@@ -1,9 +0,0 @@
----
-apiVersion: v1
-kind: Secret
-metadata:
-  name: ingress-cert
-  namespace: dcm4che
-type: Opaque
-data:
-  ca.crt: {{CERT}}
